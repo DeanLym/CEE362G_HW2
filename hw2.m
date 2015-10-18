162415e2a8410a51acfc235af5114219dd3dae23
DeanLym <yiminliu@stanford.edu>
2015-10-18 12:13:22 -0700

    Add hw2.m

--- hw2.m
+++ hw2.m
@@ -1,4 +1,21 @@
 function [] = hw2()
+    data = load('2015Assign1_1.txt');
+    t = data(:,1);
+    y = data(:,2);
+    % number of unknowns and number of observations
+    m = 500; n = 150;
+    n = 200;
+    H = shaw(n); 
+    X = ones(n,1);
+    q1();
+end
+
+
+function [] = q1()
+
+
+
+
 
 end
 
